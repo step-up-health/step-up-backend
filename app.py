aa77d9cf0dba0d6baebaa41634775668f02fab8a
jneubrand <johannes_n@icloud.com>
2016-01-31 00:22:25 -0800

    :rainbow: friend request acception code stuf

--- app.py
+++ app.py
@@ -181,6 +181,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                 data[uid]['friends'] += friendUUID
                 data[friendUUID]['friendReqs'].remove(uid)
                 data[uid]['friendReqs'].remove(friendUUID)
+                return (200, 'OK')
             else:
                 # Let's go make some friends!
                 data[friendUUID]['friendReqs'] += [uid]
