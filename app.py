a9f0f4a83f8c5dda788d59ed8e12503b98f0836e
jneubrand <johannes_n@icloud.com>
2016-02-04 14:18:25 -0800

    Use only spaces as whitespace.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import urllib.request, urllib.parse
 import random
 import json
 
+allwhitespace = u"\u0009\u000A\u000B\u000C\u000D\u0020\u0085\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200B\u2028\u2029\u202F\u205F\u3000\u180E\u200B\u200C\u200D\u2060\uFEFF"
 
 class RequestHandler(BaseHTTPRequestHandler):
     def username_in_use(self, username):
@@ -83,7 +84,10 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def set_username(self, uid, username):
         try:
-            username = username.lower().strip()
+            username = username.lower()
+            for char in allwhitespace:
+                username = username.replace(char, ' ')
+            username = username.strip()
             data = self.get_data()
             if len(username) < 1:
                 return (400, 'Username too short (min 1 character)')
